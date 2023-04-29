e4776a380ffbb8ea60b22a62cdf590ae9ff8d588
david-s-kim <mycodingtool9@gmail.com>
2023-04-29 14:17:45 +0900

    뉴스 앱

---
+++ app/src/main/java/com/example/part2_chapter5/MainActivity.kt
@@ -0,0 +1,55 @@
+package com.example.part2_chapter5
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.part2_chapter5.databinding.ActivityMainBinding
+import com.tickaroo.tikxml.TikXml
+import com.tickaroo.tikxml.retrofit.TikXmlConverterFactory
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding:ActivityMainBinding
+    private lateinit var newsAdapter: NewsAdapter
+    private val retrofit = Retrofit.Builder()
+        .baseUrl("https://news.google.com/")
+        .addConverterFactory(
+            TikXmlConverterFactory.create(
+                TikXml.Builder()
+                    .exceptionOnUnreadXml(false)
+                    .build()
+            )
+        ).build()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        newsAdapter = NewsAdapter()
+
+        binding.newsRecyclerView.apply {
+            layoutManager = LinearLayoutManager(context)
+            adapter = newsAdapter
+        }
+
+        val newsService = retrofit.create(NewsService::class.java)
+        newsService.mainFeed().enqueue(object : Callback<NewsRss>{
+            override fun onResponse(call: Call<NewsRss>, response: Response<NewsRss>) {
+                Log.e("MainActivity", "${response.body()?.channel?.items}")
+
+                newsAdapter.submitList(response.body()?.channel?.items.orEmpty())
+            }
+
+            override fun onFailure(call: Call<NewsRss>, t: Throwable) {
+                t.printStackTrace()
+            }
+        })
+
+    }
+}
